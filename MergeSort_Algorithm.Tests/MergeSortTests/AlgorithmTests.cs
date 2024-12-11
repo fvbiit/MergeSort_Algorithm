4c04d6022b13101b2791a9bfc6c9f65b18f52c66
fvbiit <ftschuggmall@gmail.com>
2024-12-11 20:59:13 +0100

    Test Cases, Commenting, Code adaptation

--- MergeSort_Algorithm.Tests/MergeSortTests/AlgorithmTests.cs
+++ MergeSort_Algorithm.Tests/MergeSortTests/AlgorithmTests.cs
@@ -5,13 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using MergeSort_Algorithm.MergeSort;
 
-
+/*This code section tests the functionality of the merge-sort algorithm using individual tests with Xunit.*/
 namespace MergeSort_Algorithm.Tests.MergeSortTests
 {
     public class AlgorithmTests
     {
+
+        //Tests the merge-sort algorithm using a normal unsorted array.
         [Fact]
-        public void DivideMergeSort_UnsortedList_ReturnsSortedList()
+        public void DivideMergeSort_UnsortedList_ReturnsSortedArray()
         {
             var alg = new Algorithm();
             int[] input = { 10, 5, 30, 15, 7 };
@@ -22,8 +24,9 @@ namespace MergeSort_Algorithm.Tests.MergeSortTests
             Assert.Equal(expected, input);
         }
 
+        //Tests the merge-sort algorithm using an array with a value.
         [Fact]
-        public void DivideMergeSort_OneValueList_ReturnsOneValueList()
+        public void DivideMergeSort_OneValueList_ReturnsOneValueArray()
         {
             var alg = new Algorithm();
             int[] input = {7};
@@ -34,8 +37,9 @@ namespace MergeSort_Algorithm.Tests.MergeSortTests
             Assert.Equal(expected, input);
         }
 
+        //Tests the merge-sort algorithm using an empty array.
         [Fact]
-        public void DivideMergeSort_EmptyList_ReturnsEmptyList()
+        public void DivideMergeSort_EmptyList_ReturnsEmptyArray()
         {
             var alg = new Algorithm();
             int[] input =  { };
@@ -46,8 +50,44 @@ namespace MergeSort_Algorithm.Tests.MergeSortTests
             Assert.Equal(expected, input);
         }
 
+        //Tests the merge-sort algorithm using a sorted array.
+        [Fact]
+        public void DivideMergeSort_EmptyList_ReturnsAlreadySortedArray()
+        {
+            var alg = new Algorithm();
+            int[] input = { 1, 2, 3, 4, 5 };
+            int[] expected = { 1, 2, 3, 4, 5 };
 
+            alg.DivideMergeSort(input, 0, input.Length - 1);
 
+            Assert.Equal(expected, input);
+        }
+
+        //Tests the merge-sort algorithm using an unsorted array with duplicates.
+        [Fact]
+        public void DivideMergeSort_EmptyList_ReturnsSortedArrayWithDuplicates()
+        {
+            var alg = new Algorithm();
+            int[] input = { 5, 1, 5, 12, 2, 4 };
+            int[] expected = { 1, 2, 4, 5, 5, 12 };
+
+            alg.DivideMergeSort(input, 0, input.Length - 1);
+
+            Assert.Equal(expected, input);
+        }
+
+        //Tests the merge-sort algorithm using an unsorted array with positive and negative values.
+        [Fact]
+        public void DivideMergeSort_EmptyList_ReturnsSortedArrayWithNegativeNumbers()
+        {
+            var alg = new Algorithm();
+            int[] input = { -1, -3, 5, 1, 8 };
+            int[] expected = { -3, -1, 1, 5, 8 };
+
+            alg.DivideMergeSort(input, 0, input.Length - 1);
+
+            Assert.Equal(expected, input);
+        }
 
     }
 }
--- MergeSort_Algorithm/MergeSort/Algorithm.cs
+++ MergeSort_Algorithm/MergeSort/Algorithm.cs
@@ -10,65 +10,78 @@ namespace MergeSort_Algorithm.MergeSort
     public class Algorithm
     {
 
+        /*
+         The DivideMergeSort method is the main method of the algorithm. 
+         The recursive function call theoretically breaks down the array into its smallest components but in reality, only the indices of the array are subdivided further and further. 
+         The merge method is also called in the main method and merges the array and sorts it using the indices.
+         */
         public void DivideMergeSort(int[] unsorted_values, int left_index, int right_index)
         {
             if (right_index > left_index)
             {
                 int middle_index = (left_index + right_index) / 2;
 
-                //Recursive Function Call for Left
+                //Recursive Function Call for Left index site
                 DivideMergeSort(unsorted_values, left_index, middle_index);
 
-                //Recursive Function Call for Right
+                //Recursive Function Call for Right index site
                 DivideMergeSort(unsorted_values, middle_index+1, right_index);
 
+                //Merge Method
                 Merge(unsorted_values, left_index, middle_index, right_index);
 
             }
 
         }
 
+        /*
+        The merge method takes over the unsorted array and the required indexes to merge and sort the array. 
+         */
         public void Merge(int[] unsorted_values, int left_index, int middle_index, int right_index)
         {
             int left_array_length = middle_index - left_index + 1;
             int right_array_length = right_index - middle_index;
-            int a, b, c;
+            int left_cnt, right_cnt, comparevalue_left;
 
             int[] left_array = new int[left_array_length];
             int[] right_array = new int[right_array_length];
 
-            for (a = 0; a < left_array_length; ++a)
-                left_array[a] = unsorted_values[left_index + a];
-            for (b = 0; b < right_array_length; ++b)
-                right_array[b] = unsorted_values[middle_index + 1 + b];
+            //Copies the left and right sides of the array into two temporary arrays 
+            for (left_cnt = 0; left_cnt < left_array_length; ++left_cnt)
+                left_array[left_cnt] = unsorted_values[left_index + left_cnt];
+            for (right_cnt = 0; right_cnt < right_array_length; ++right_cnt)
+                right_array[right_cnt] = unsorted_values[middle_index + 1 + right_cnt];
 
-            a = 0;
-            b = 0;
-            c = left_index;
+            left_cnt = 0;
+            right_cnt = 0;
+            comparevalue_left = left_index;
 
-            while (a < left_array_length && b < right_array_length) 
+            while (left_cnt < left_array_length && right_cnt < right_array_length) 
             { 
-                if (left_array[a] <= right_array[b])
+                if (left_array[left_cnt] <= right_array[right_cnt])
                 {
-                    unsorted_values[c++] = left_array[a++];
+                    unsorted_values[comparevalue_left++] = left_array[left_cnt++];
                 }
                 else
                 {
-                    unsorted_values[c++] = right_array[b++];
+                    unsorted_values[comparevalue_left++] = right_array[right_cnt++];
                 }
             }
 
-            while (a < left_array_length)
+            while (left_cnt < left_array_length)
             {
-                unsorted_values[c++] = left_array[a++];
+                unsorted_values[comparevalue_left++] = left_array[left_cnt++];
             }
 
-            while (b < right_array_length)
+            while (right_cnt < right_array_length)
             {
-                unsorted_values[c++] = right_array[b++]; 
+                unsorted_values[comparevalue_left++] = right_array[right_cnt++]; 
             }
         }
 
+        /*
+         This is the main function that executes the programm and display the output.
+         */
         public static void Main(string[] args)
         {   
             var alg = new Algorithm();  
