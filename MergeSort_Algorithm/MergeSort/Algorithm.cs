2ac077c0315bb14e5e09c5b0d6846284eb4d2f51
fvbiit <ftschuggmall@gmail.com>
2024-12-13 20:48:07 +0100

    Code Commenting

--- MergeSort_Algorithm/MergeSort/Algorithm.cs
+++ MergeSort_Algorithm/MergeSort/Algorithm.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
 
 namespace MergeSort_Algorithm.MergeSort
 {
@@ -39,23 +41,32 @@ namespace MergeSort_Algorithm.MergeSort
          */
         public void Merge(int[] unsorted_values, int left_index, int middle_index, int right_index)
         {
+            // Variable declaration and definition
             int left_array_length = middle_index - left_index + 1;
             int right_array_length = right_index - middle_index;
             int left_cnt, right_cnt, comparevalue_left;
 
+            // Create temp Arrays for left and right half
             int[] left_array = new int[left_array_length];
             int[] right_array = new int[right_array_length];
 
-            //Copies the left and right sides of the array into two temporary arrays 
-            for (left_cnt = 0; left_cnt < left_array_length; ++left_cnt)
+            //Copies the left and right sites of the array into two temporary arrays 
+            for (left_cnt = 0; left_cnt < left_array_length; ++left_cnt) {
+                
                 left_array[left_cnt] = unsorted_values[left_index + left_cnt];
-            for (right_cnt = 0; right_cnt < right_array_length; ++right_cnt)
-                right_array[right_cnt] = unsorted_values[middle_index + 1 + right_cnt];
+            }
 
+            for (right_cnt = 0; right_cnt < right_array_length; ++right_cnt) { 
+
+                right_array[right_cnt] = unsorted_values[middle_index + 1 + right_cnt];
+            }
+
+            //Initialize
             left_cnt = 0;
             right_cnt = 0;
             comparevalue_left = left_index;
 
+            // Copy elements from both arrays sorted back into the original array
             while (left_cnt < left_array_length && right_cnt < right_array_length) 
             { 
                 if (left_array[left_cnt] <= right_array[right_cnt])
@@ -68,11 +79,13 @@ namespace MergeSort_Algorithm.MergeSort
                 }
             }
 
+            // Copy remaining elements of the left array
             while (left_cnt < left_array_length)
             {
                 unsorted_values[comparevalue_left++] = left_array[left_cnt++];
             }
 
+            // Copy remaining elements of the right array
             while (right_cnt < right_array_length)
             {
                 unsorted_values[comparevalue_left++] = right_array[right_cnt++]; 
@@ -80,15 +93,19 @@ namespace MergeSort_Algorithm.MergeSort
         }
 
         /*
-         This is the main function that executes the programm and display the output.
+         This is the main function that executes the programm and displays the output.
          */
         public static void Main(string[] args)
         {   
+            // Create Object of Algorithm Class
             var alg = new Algorithm();  
+
+            //Unsorted Values before Merge Sort
             int[] unsorted_values = { 10, 5, 30, 15, 7 };
             Console.WriteLine("Your given array is");
             Console.WriteLine(string.Join(" ", unsorted_values));
 
+            //Sorted Values after Merge Sort
             alg.DivideMergeSort(unsorted_values, 0, unsorted_values.Length - 1);
 
             Console.WriteLine("\nYour Sorted array is");
